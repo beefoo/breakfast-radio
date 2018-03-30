efe85e830b30ffc9c66800f97a6a8832b8865510
Brian Foo <brian@youaremyjoy.org>
2018-03-30 17:39:12 -0400

    add data viz

--- scripts/process_audio.py
+++ scripts/process_audio.py
@@ -12,11 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_DIR", default="../audio/", help="Input directory")
 parser.add_argument('-manifest', dest="MANIFEST_FILE", default="manifest.csv", help="Input manifest csv file")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="../js/manifest.js", help="Javascript output file")
+parser.add_argument('-viz', dest="VIZ", default=0, help="Visualize output")
 
 args = parser.parse_args()
 INPUT_DIR = args.INPUT_DIR
 MANIFEST_FILE = args.MANIFEST_FILE
 OUTPUT_FILE = args.OUTPUT_FILE
+VIZ = (args.VIZ > 0)
 
 files = []
 with open(MANIFEST_FILE, 'rb') as f:
@@ -38,6 +40,46 @@ for i, f in enumerate(files):
     files[i]["minute"] = minute
     files[i]["duration"] = duration
     files[i].pop("time", None)
+
+if VIZ:
+    import math
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    barH = 10
+    yticks = []
+
+    for i, f in enumerate(files):
+        files[i]["start"] = f["hour"] * 60 + f["minute"] + f["zone"] * 60
+        files[i]["end"] = files[i]["start"] + f["duration"] / 60.0
+    files = sorted(files, key=lambda k: k['start'])
+
+    fig, ax = plt.subplots()
+    for i, f in enumerate(files):
+        start = f["start"]
+        duration = f["duration"] / 60.0
+        ax.broken_barh([(start, duration)], (barH*(i+1), barH-2), facecolors='blue')
+        yticks.append(barH*(i+1) + barH/2)
+
+    def ceilToNearest(x, nearest):
+        return int(math.ceil(1.0 * x / nearest)) * nearest
+
+    def floorToNearest(x, nearest):
+        return int(math.floor(1.0 * x / nearest)) * nearest
+
+    start = floorToNearest(min([f["start"] for f in files]), 60)
+    end = ceilToNearest(max([f["end"] for f in files]), 60)
+    ax.set_xticks(np.arange(start, end, 60))
+    ax.set_yticks(yticks)
+
+    ylabels = [f["filename"].replace(".mp3", "") for f in files]
+    ax.set_yticklabels(ylabels)
+    ax.grid(True)
+
+    plt.show()
+
+    sys.exit(1)
+
 
 jsonString = "var MANIFEST = " + json.dumps(files, indent=2, sort_keys=True) + ";"
 
